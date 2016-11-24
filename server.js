963adf4e2cd6ba76ef716351f5004b4946fdc1fd
LuciOroles <stoica.florin.lucian@gmail.com>
2016-11-24 10:44:12 +0200

    adding the pathID parameter

--- server.js
+++ server.js
@@ -2,27 +2,25 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8257;
 var request = require('request-promise');
-
-
-
 app.get('/api', function(req, res) {
  	
  	var theId =req.query.id;
- 	console.log( req.query.id + "# "  + 		req.query.person_id  + "#" +
- 		 			req.query.name + "#" +
- 			req.query.emailAddress+ "#" +
- 			req.query.tags + "#" );
+ 	//console.log( req.query.id + "# "  + 		req.query.person_id  + "#" +  		 			req.query.name + "#" + 			req.query.emailAddress+ "#" +  			req.query.tags + "#" );
+ 	var pathId= req.query.pathID || "13";
 
+ 	//console.log(pathId);
 	//console.log(theId + " id pushed");
-	//	http://localhost:8257/api/pjtu9965336/22
-	//http://localhost:8257/api?id=pjtu9965341&person_id=1&name=Gigel&emailAddress=mail@mail.com&tags=test
+	//http://localhost:8257/api/pjtu9965336/22
+	//http://localhost:8257/api?id=pjtu9965364&person_id=1&name=Georgel&emailAddress=mail@mail.com&tags=test&pathID=13
+	//https://pure-taiga-37878.herokuapp.com/api?id=pjtu9965346&person_id=1&name=Gigel&emailAddress=mail@mail.com&tags=test&pathID=13
+  //https://ldm.nurago.com/api/v1/dmi.tt/source-panels/13/members"
 	var options = {
-		uri : 'https://ldm.nurago.com/api/v1/dmi.tt/source-panels/13/members?apikey=OTXMl4ANNqHtIxIwm7deqIcdAYQvB4QJXNp7OMX6aLzSwJbPtUjlE2GUDThrPr2KOeNWene5TPmpXKEaZ7AZtlHX4Kqu2cQXAk7I',
+		uri : 'https://ldm.nurago.com/api/v1/dmi.tt/source-panels/'+ pathId +'/members?apikey=OTXMl4ANNqHtIxIwm7deqIcdAYQvB4QJXNp7OMX6aLzSwJbPtUjlE2GUDThrPr2KOeNWene5TPmpXKEaZ7AZtlHX4Kqu2cQXAk7I&apikey=OTXMl4ANNqHtIxIwm7deqIcdAYQvB4QJXNp7OMX6aLzSwJbPtUjlE2GUDThrPr2KOeNWene5TPmpXKEaZ7AZtlHX4Kqu2cQXAk7I',
 		method: 'POST' ,
-	 	json: true, //"pjtu9965332"
+	 	json: true, 
         body : {
                     "household_id": theId ,
-                     "person_id": req.query.person_id,
+                    "person_id": req.query.person_id,
                     "policy_accepted": true,
                     "name": req.query.name,
                     "attributes": {
@@ -30,26 +28,18 @@ app.get('/api', function(req, res) {
                     },
                     "tags": req.query.tags
                 }
- 
-	};
-
+  	};
  	request(options).then(function (response) {
-    // Request was successful, use the response object at will
-     //console.log(response);
+    // delete response.source_panel;
+     console.log( response.internal_id);
      res.json(response);
   })
-  .catch(function (err, response, body) {
-    // Something bad happened, handle the error
-	//    console.log(err);
-
-		console.log(response);
-      res.status(404).json({
-                     error : err.statusCode
+  .catch(function (err) {
+        res.status(404).json({
+                     error : err
                  });
   });
 });
-
-
 app.listen(PORT, function() {
     console.log("express listening on port " + PORT + "!");
 });
